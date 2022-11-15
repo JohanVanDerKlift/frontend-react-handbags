32e6294227681f389f9aa2a991de544437a2befd
Klippie <info@johanvanderklift.nl>
2022-11-15 17:41:18 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  function buttonClick() {
+    console.log("Button clicked")
+  }
+
   return (
+    <>
       <h1>Handbags & Purses</h1>
+      <nav>
+        <button type="button" onClick={buttonClick}>to the collection</button>
+        <button type="button" onClick={buttonClick}>shop all bags</button>
+        <button type="button" onClick={buttonClick} disabled>pre-order</button>
+      </nav>
+    </>
   );
 }
 
